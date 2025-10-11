4e20b67f1c3f446c6cb67fc6854e049aef891cd5
DaniielDz <daniel13cba2002@gmail.com>
2025-10-11 11:19:40 -0300

    feat(app): add errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes";
 import cookieParser from "cookie-parser";
 import { authMiddleware } from "./middlewares/auth";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(authMiddleware);
 app.use("/", routes);
+app.use(errorHandler);
 
 export default app;
